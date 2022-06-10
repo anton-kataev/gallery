b85268b815dfb3aa86d26e2529f6e31d4a1d0adb
Anton Kataev <a.v.kataev.it@gmail.com>
2022-06-10 08:10:58 +0300

    Start project

---
+++ src/router.ts
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+
+const routes: Array<RouteRecordRaw> = [
+    {
+        path: '/',
+        name: 'home',
+        component: () => import('./pages/index.vue')
+    },
+    {
+        path: '/about',
+        name: 'about',
+        component: () => import('./pages/about.vue')
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router
